92d59452228ebebc1d31137e4e1c1cc087feabf0
Glitch (slack-tuto-approval-workflow) <none>
2022-01-26 14:38:04 +0000

    📶®️ Updated with Glitch

---
+++ src/api.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const axios = require('axios');
+const payloads = require('./payloads');
+const apiUrl = 'https://slack.com/api';
+
+/**
+ * helper function to call POST methods of Slack API
+ */
+const callAPIMethodPost = async (method, payload) => {
+  let result = await axios.post(`${apiUrl}/${method}`, payload, {
+    headers: { Authorization: "Bearer " + process.env.SLACK_ACCESS_TOKEN }
+  });
+  return result.data;
+}
+
+/**
+ * helper function to call GET methods of Slack API
+ */
+const callAPIMethodGet = async (method, payload) => {
+  payload.token = process.env.SLACK_ACCESS_TOKEN
+  let params = Object.keys(payload).map(key => `${key}=${payload[key]}`).join('&')
+  let result = await axios.get(`${apiUrl}/${method}?${params}`);
+  return result.data;
+}
+
+/**
+ * helper function to receive all channels our bot user is a member of
+ */
+const getChannels = async (userId, channels, cursor) => {
+  channels = channels || []
+  console.log ("getChannels");
+  console.log ("userId "+userId);
+  console.log ("channels "+channels);
+  let payload = {}
+  if (cursor) payload.cursor = cursor
+  console.log ("cursor "+cursor);
+  let result = await callAPIMethodPost('users.conversations', payload)
+  console.log ("result "+result);
+  console.log ("result.conversations "+result.conversations);
+  console.log ("result.channels "+result.channels);
+  channels = channels.concat(result.channels)
+  console.log ("channels "+channels);
+  if (result.response_metadata && result.response_metadata.next_cursor && result.response_metadata.next_cursor.length)
+    return getChannels(userId, channels, result.response_metadata.next_cursor)
+
+  return channels
+}
+
+const requestAnnouncement = async (user, submission) => {
+  // Send the approver a direct message with "Approve" and "Reject" buttons 
+  let res = await callAPIMethodPost('conversations.open', {
+    users: submission.approver
+  })
+  submission.requester = user.id;
+  submission.channel = res.channel.id;
+  await callAPIMethodPost('chat.postMessage', payloads.approve(submission));
+};
+
+const rejectAnnouncement = async (payload, announcement) => {
+  // 1. update the approver's message that this request has been denied
+  await callAPIMethodPost('chat.update', {
+    channel: payload.channel.id,
+    ts: payload.message.ts,
+    text: 'This request has been denied. I am letting the requester know!',
+    blocks: null
+  });
+
+  // 2. send a notification to the requester
+  let res = await callAPIMethodPost('conversations.open', {
+    users: announcement.requester
+  })
+  await callAPIMethodPost('chat.postMessage', payloads.rejected({
+    channel: res.channel.id,
+    title: announcement.title,
+    details: announcement.details,
+    channelString: announcement.channelString
+  }));
+}
+
+const postAnnouncement = async (payload, announcement) => {
+  await callAPIMethodPost('chat.update', {
+    channel: payload.channel.id,
+    ts: payload.message.ts,
+    text: 'Thanks! This post has been announced.',
+    blocks: null
+  });
+
+  announcement.channels.forEach(channel => {
+    callAPIMethodPost('chat.postMessage', payloads.announcement({
+      channel: channel,
+      title: announcement.title,
+      details: announcement.details,
+      requester: announcement.requester,
+      approver: announcement.approver
+    }));
+  })
+}
+
+
+
+module.exports = {
+  callAPIMethodPost,
+  callAPIMethodGet,
+  getChannels,
+  rejectAnnouncement,
+  postAnnouncement,
+  requestAnnouncement
+}
